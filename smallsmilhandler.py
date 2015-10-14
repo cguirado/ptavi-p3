351534b6b93eeca48288d4d08ff45780c524c841
c.guirado <cgboluda@gmail.com>
2015-10-14 18:24:06 +0200

    corrigiendo errores tag

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -15,11 +15,14 @@ class SmallSMILHandler(ContentHandler):
         self.right = ""
         self.src_img = ""
         self.src_audio = ""
-        self.region = ""
+        self.region_img = ""
         self.begin_img = ""
         self.begin = ""
         self.dur = ""
-        self.regin=""
+        self.dur_img = ""
+        self.region = ""
+        self.id = ""
+        self.src = ""
 #No necesitamos endElement (solo tenemos principio de etiqueta)
     def startElement(self,name,attrs):
         if name == "root-layout":
@@ -34,24 +37,25 @@ class SmallSMILHandler(ContentHandler):
             self.left = attrs.get(left,"")
             self.right = attrs.get(right,"")
         if name == "img":
-            self.src_img = attrs.get(src_img,"")
-            self.region = attrs.get(region,"")
-            self.begin_img = attrs.get(begin_img,"")
-            self.dur = attrs.get(dur,"")
+            self.src_img = attrs.get(src,"")
+            self.region_img = attrs.get(region,"")
+            self.begin_img = attrs.get(begin,"")
+            self.dur_img = attrs.get(dur,"")
         if name == "audio":
-            self.src_audio = attrs.get(src_audio,"")
+            self.src_audio = attrs.get(src,"")
             self.begin = attrs.get(begin, "")
-            self.dur = attrs.get()
+            self.dur = attrs.get(dur,"")
         if name == "textstream":
-            self.src = attrs.get()
-            self.regin = attrs.get()
+            self.src = attrs.get(src,"")
+            self.region = attrs.get(region,"")
     def get_tags (self):
         tagroot = {"width": self.width, "height": self.height, "backgroundcolor": self.backgroundcolor}
         tagregion= {"id": self.id, "top": self.top, "botton": self.botton, "left": self.left, "right": self.right}
-        tagimg = {"src_img": self.src_img, "region": self.region, "begin": self.begin, "dur":self.dur}
-        tagaudio = {"src_audio": self.src_audio. "begin": self.begin, "dur": self.dur}
-        tagtext = {"src": self.src, "regin": self.regin}
+        tagimg = {"src": self.src_img, "region": self.region_img, "begin": self.begin_img, "dur":self.dur_img}
+        tagaudio = {"src": self.src_audio, "begin": self.begin, "dur": self.dur}
+        tagtext = {"src": self.src, "region": self.regin}
         self.tag = [tagroot, tagregion, tagimg, tagaudio, tagtext]
+        return self.tag
 if __name__ == "__main__":
     """
     Programa principal
@@ -59,5 +63,6 @@ if __name__ == "__main__":
     parser = make_parser()
     cHandler = SmallSMILHandler()
     #parser.setContentHandler(cHandler)
-    datos = cHandlet.get_tags()
+    datos = cHandler.get_tags()
     parser.parse(open('karaoke.smil'))
+    print(datos)
