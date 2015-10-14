128c99fa32941d708dff93ade9277762b980e3f3
c.guirado <cgboluda@gmail.com>
2015-10-14 22:59:03 +0200

    hecho bucle imprime vacio

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,26 +7,22 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
     def __init__ (self):
         self.tags = []
-        self.diccsmil = {"root-layout":['width', 'height', 'background-color'],
+        self.dicc = {"root-layout":['width', 'height', 'background-color'],
                     "region":['id', 'top', 'left', 'bottom', 'right'],
                     "img":['src', 'region', 'begin', 'dur'],
                     "audio":['src', 'begin', 'dur'],
                     "textstream":['src','region']}
 #No necesitamos endElement (solo tenemos principio de etiqueta)
     def startElement(self, name, attrs):
-        if name in self.diccsmil
+        if name in self.dicc:
             dicc2={}
             #Empiezo a meterme dentro del diccsmil para sacar atributos segun nombre
-            for atributo in self.diccsmil[name]
+            for atributo in self.dicc[name]:
                 #introduzco en mi nuevo diccionario los atributos que voy leyendo
                 dicc2[atributo] = attrs.get('atributo',"")
             #introduzco en mi lista tag lo que voy a acumulando con nombre y dicc
             self.tags.append([name,dicc2])
-
-
-
-
-                    """
+    """
         if name == "root-layout":
         #valores de los atributos"
             self.width = attrs.get('width',"")
@@ -70,6 +66,7 @@ if __name__ == "__main__":
     """
     parser = make_parser()
     cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)parser.parse(open('karaoke.smil'))
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
     datos = cHandler.get_tags()
     print(datos)
