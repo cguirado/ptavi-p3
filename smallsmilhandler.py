e7a2fe02da4ed70c33ba6af35a54dd7451d9bc15
c.guirado <cgboluda@gmail.com>
2015-10-14 22:46:04 +0200

    hecho bucle probar

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,33 +6,27 @@ from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
     def __init__ (self):
-        self.width = ""
-        self.height = ""
-        self.backgroundcolor = ""
-        self.id = ""
-        self.top = ""
-        self.botton = ""
-        self.left = ""
-        self.right = ""
-        self.src_img = ""
-        self.region_img = ""
-        self.begin_img = ""
-        self.dur_img = ""
-        self.src_audio = ""
-        self.begin = ""
-        self.dur = ""
-        self.src = ""
-        self.region = ""
-
         self.tags = []
-        self.dicc = {"root-layout":['width', 'height', 'background-color'],
+        self.diccsmil = {"root-layout":['width', 'height', 'background-color'],
                     "region":['id', 'top', 'left', 'bottom', 'right'],
                     "img":['src', 'region', 'begin', 'dur'],
                     "audio":['src', 'begin', 'dur'],
                     "textstream":['src','region']}
 #No necesitamos endElement (solo tenemos principio de etiqueta)
     def startElement(self, name, attrs):
-        """
+        if name in self.diccsmil
+            dicc2={}
+            #Empiezo a meterme dentro del diccsmil para sacar atributos segun nombre
+            for atributo in self.diccsmil[name]
+                #introduzco en mi nuevo diccionario los atributos que voy leyendo
+                dicc2[atributo] = attrs.get('atributo',"")
+            #introduzco en mi lista tag lo que voy a acumulando con nombre y dicc
+            self.tags.append([name,dicc2])
+
+
+
+
+                    """
         if name == "root-layout":
         #valores de los atributos"
             self.width = attrs.get('width',"")
