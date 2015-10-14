eacbffb83c0415664926570fd26a59d6601a1413
c.guirado <cgboluda@gmail.com>
2015-10-14 18:10:23 +0200

    introducido tag

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -13,10 +13,13 @@ class SmallSMILHandler(ContentHandler):
         self.botton = ""
         self.left = ""
         self.right = ""
-        self.src = ""
+        self.src_img = ""
+        self.src_audio = ""
         self.region = ""
+        self.begin_img = ""
         self.begin = ""
         self.dur = ""
+        self.regin=""
 #No necesitamos endElement (solo tenemos principio de etiqueta)
     def startElement(self,name,attrs):
         if name == "root-layout":
@@ -31,19 +34,24 @@ class SmallSMILHandler(ContentHandler):
             self.left = attrs.get(left,"")
             self.right = attrs.get(right,"")
         if name == "img":
-            self.src = attrs.get(src,"")
+            self.src_img = attrs.get(src_img,"")
             self.region = attrs.get(region,"")
-            self.begin = attrs.get(begin,"")
+            self.begin_img = attrs.get(begin_img,"")
             self.dur = attrs.get(dur,"")
         if name == "audio":
-            self.src = attrs.get()
-            self.begin = attrs.get()
+            self.src_audio = attrs.get(src_audio,"")
+            self.begin = attrs.get(begin, "")
             self.dur = attrs.get()
         if name == "textstream":
             self.src = attrs.get()
             self.regin = attrs.get()
     def get_tags (self):
-        return (self.tag)
+        tagroot = {"width": self.width, "height": self.height, "backgroundcolor": self.backgroundcolor}
+        tagregion= {"id": self.id, "top": self.top, "botton": self.botton, "left": self.left, "right": self.right}
+        tagimg = {"src_img": self.src_img, "region": self.region, "begin": self.begin, "dur":self.dur}
+        tagaudio = {"src_audio": self.src_audio. "begin": self.begin, "dur": self.dur}
+        tagtext = {"src": self.src, "regin": self.regin}
+        self.tag = [tagroot, tagregion, tagimg, tagaudio, tagtext]
 if __name__ == "__main__":
     """
     Programa principal
@@ -51,5 +59,5 @@ if __name__ == "__main__":
     parser = make_parser()
     cHandler = SmallSMILHandler()
     #parser.setContentHandler(cHandler)
+    datos = cHandlet.get_tags()
     parser.parse(open('karaoke.smil'))
-    print(self.width)
