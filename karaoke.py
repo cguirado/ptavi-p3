ab47514127572d66e693bf64c1688d3c266c8049
c.guirado <cgboluda@gmail.com>
2015-10-16 19:00:27 +0200

    4.4.1 hecho error fichero

---
+++ karaoke.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
+
+
+if __name__ == "__main__":
+
+    comandos = sys.argv
+    print (comandos)
+    print(len(comandos))
+    if len(comandos) != 2:
+        print ("Usage: python3 karaoke.py file.smil")
