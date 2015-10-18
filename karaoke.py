4824be0bae625abc72dffd537b8d9ebbf12a7d88
c.guirado <cgboluda@gmail.com>
2015-10-18 17:01:38 +0200

    introduciendo apartado6

--- karaoke.py
+++ karaoke.py
@@ -8,34 +8,43 @@ import smallsmilhandler
 import os
 
 
-if __name__ == "__main__":
+class karaokeLocal():
+    def __init__ (self):
+        comandos = sys.argv
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        if len(comandos) != 2:
+            print ("Usage: python3 karaoke.py file.smil")
+        else:
+            parser.parse(open(comandos[1]))
+            self.datos = cHandler.get_tags()
 
+    def __str__(self):
+        total = ""
+        for linea in self.datos:
+            name = linea[0] + '\T'
+            atributos = ""
+            for atributo in linea[1].keys():
+                if linea[1][atributo] != "":
+                    elemento = linea[1][atributo]
+                    atributos = atributos + atributo
+                    atributos += '=' + elemento + '\T'
+            total += name + atributos + '\n'
+        print (total)
+
+if __name__ == "__main__":
+    karaoke =
+
+
+
+
+
+"""
     comandos = sys.argv
     parser = make_parser()
     cHandler = smallsmilhandler.SmallSMILHandler()
     parser.setContentHandler(cHandler)
-
-
-    if len(comandos) != 2:
-        print ("Usage: python3 karaoke.py file.smil")
-    else:
-        parser.parse(open(comandos[1]))
-        datos = cHandler.get_tags()
-        total = ""
-        for linea in datos:
-            #lo primero que queremos el nombre mas el \t
-            name = linea[0] + '\T'
-            atributos = "" #Luego iremos mentiendo elementos
-            #queremos es ir cogiendo los atributos tras coger el nombre e ir quitando blancos.
-            #lina[1] (diccionario) como anteriormente atributo (primera parte del atributo)
-            for atributo in linea[1].keys():
-                if linea[1][atributo] != "":
-                    elemento = linea[1][atributo]#sacamos el valor del atributo
-                    #print(elemento)
-                    atributos = atributos + atributo
-                    atributos += '=' + elemento + '\T' #Unimos mas cosas a nuestra lista
-            total += name + atributos + '\n'#nombre mas atributo completo con su valor 2 listas diferentes
-        #print (total)
 
         for linea in datos:
             for atributo in linea[1].keys():
