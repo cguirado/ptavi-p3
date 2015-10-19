0cbd8f95ebdf33f0c8a9ec84229c739e8d468138
c.guirado <cgboluda@gmail.com>
2015-10-19 13:37:03 +0200

    Descarga okpython3 karaoke.py karaoke.smilpython3 karaoke.py karaoke.smil

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,33 +4,28 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-class SmallSMILHandler(ContentHandler):
-    def __init__ (self):
-        self.tags = []
-        self.dicc = {"root-layout":['width', 'height', 'background-color'],
-                    "region":['id', 'top', 'left', 'bottom', 'right'],
-                    "img":['src', 'region', 'begin', 'dur'],
-                    "audio":['src', 'begin', 'dur'],
-                    "textstream":['src','region']}
 
-#No necesitamos endElement (solo tenemos principio de etiqueta)
+class SmallSMILHandler(ContentHandler):
+    def __init__(self):
+        self.tags = []
+        self.dicc = {"root-layout": ['width', 'height', 'background-color'],
+                     "region": ['id', 'top', 'left', 'bottom', 'right'],
+                     "img": ['src', 'region', 'begin', 'dur'],
+                     "audio": ['src', 'begin', 'dur'],
+                     "textstream": ['src', 'region']}
+
     def startElement(self, name, attrs):
         if name in self.dicc:
             dicc2 = {}
-            #Empiezo a meterme dentro del diccsmil para sacar atributos segun nombre
             for atributo in self.dicc[name]:
-                #introduzco en mi nuevo diccionario los atributos que voy leyendo
-                dicc2[atributo] = attrs.get(atributo,"")
-            #introduzco en mi lista tag lo que voy a acumulando con nombre y dicc
-            self.tags.append([name,dicc2])
-            print("")
+                dicc2[atributo] = attrs.get(atributo, "")
+            self.tags.append([name, dicc2])
 
-    def get_tags (self):
+    def get_tags(self):
         return self.tags
+
 if __name__ == "__main__":
-    """
-    Programa principal
-    """
+
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
