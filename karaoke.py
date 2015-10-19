dbd5047943056dee238fc2fd1a7dab170af7d4f7
c.guirado <cgboluda@gmail.com>
2015-10-19 10:45:12 +0200

    falta json

--- karaoke.py
+++ karaoke.py
@@ -5,8 +5,7 @@ import sys
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
-import os
-
+import urllib
 
 class KaraokeLocal():
     def __init__ (self):
@@ -21,29 +20,34 @@ class KaraokeLocal():
     def __str__(self):#Imprimir como pide
         total = ""
         for linea in self.datos:
-            name = linea[0] + '\T'
+            name = linea[0] + '\t'
             atributos = ""
             for atributo in linea[1].keys():
                 if linea[1][atributo] != "":
                     elemento = linea[1][atributo]
                     atributos = atributos + atributo
-                    atributos += '=' + elemento + '\T'
+                    atributos += '=' + elemento + '\t'
             total += name + atributos + '\n'
         return (total)
 
     def do_local(self):#Descargar
-        for linea in datos:
+        for linea in self.datos:
             for atributo in linea[1].keys():
                 if atributo == "src":#ESto me lo hace bien
                     if linea[1][atributo].split(':')[0] == "http":
-                        os.system( "wget -q" + linea[1][atributo])
+                        urllib.request.urlretrieve(linea[1][atributo],sys.argv[1])
+    """
+    def do_json(self):
+        principio = sys.argv[1].split('.')
+        if principio[-1] == 'smil'
 
+    """
 if __name__ == "__main__":
     comandos = sys.argv
     if len(comandos) != 2:
         print ("Usage: python3 karaoke.py file.smil")
     else:
         karaoke = KaraokeLocal()
-        print (karaoke.__str__())
-        karaoke.do_local
-        print(karaoke.__str__)
+        print (karaoke)
+        karaoke.do_local()
+        print(karaoke)
